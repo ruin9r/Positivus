b2cbad4a8ac782a6047f538163a244a74d337d11
a.kokora <andrey18121996@gmail.com>
2026-06-10 01:02:03 +0300

    create: hero section + adaptive

--- index.js
+++ index.js
@@ -9,3 +9,29 @@ burgerBtn.addEventListener('click', () => {
   overlay.classList.toggle("overlay--active");
   body.classList.toggle("page--blocked");
 })
+
+const swiper = new Swiper ('.hero__swiper', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  loop: true,
+  speed: 9000,
+  spaceBetween: 37,
+  autoplay: {
+    delay: 0,
+  }, 
+  breakpoints: {
+    442: {
+      slidesPerView: 2,
+      spaceBetween: 37,
+    },
+    700: {
+      slidesPerView: 4,
+      spaceBetween: 57,
+    },
+    1000: {
+      slidesPerView: 6,
+      spaceBetween: 95,
+    }
+    
+  }
+})
